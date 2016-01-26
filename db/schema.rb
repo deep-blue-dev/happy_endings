450770eef17120378cc90c4ce83080c172fdbbfa
Volney Tejeda <volneytejeda10@gmail.com>
2016-01-26 17:08:16 -0500

    closes #18

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160125175753) do
+ActiveRecord::Schema.define(version: 20160126215946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 20160125175753) do
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
 
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "portfolios", force: :cascade do |t|
     t.string   "client_name"
     t.string   "picture1"
@@ -45,13 +51,16 @@ ActiveRecord::Schema.define(version: 20160125175753) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
-    t.string   "category"
     t.integer  "price"
     t.string   "image_1"
     t.string   "image_2"
     t.boolean  "promo"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
 
+  add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
+
+  add_foreign_key "products", "categories"
 end
