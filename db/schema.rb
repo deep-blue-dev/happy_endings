4b45bebafe3157bfe7b4c2683fce24effb6d7fc6
Volney Tejeda <volneytejeda10@gmail.com>
2016-01-29 17:31:03 -0500

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160126215946) do
+ActiveRecord::Schema.define(version: 20160129044307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20160126215946) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
   create_table "portfolios", force: :cascade do |t|
