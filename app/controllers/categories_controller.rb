c36ba2b0168d443cc9f3f62c7cec97e99d5dd84b
Volney Tejeda <volneytejeda10@gmail.com>
2016-02-01 00:44:52 -0500

    fix nil errors in prodcts for real this time
    hopefully

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,7 +6,7 @@ class CategoriesController < ApplicationController
   # GET /categories.json
   def index
     @categories = Category.all
-    @products = Category.all.reject{ |category| category.products.blank? }.sample.products
+    @products = Category.all.reject{ |category| category.try(:products).blank? }.sample.try(:products)
   end
 
   # GET /categories/1
